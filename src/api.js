21c757d018dcfc0b64083e7fc6649d73bdcc9777
SiMercer <na>
2025-06-27 20:37:34 +0100

    resetting fiels

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.8.150:23704/News-app-BE-api/api",
+  baseURL: "https://news-site-app.onrender.com/api",
 });
 
 const getTopics = () => {
