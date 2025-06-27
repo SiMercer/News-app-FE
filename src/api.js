9bc536b26c3e182e6eec9f81dcd8db32baeec2b7
SiMercer <na>
2025-06-27 22:21:59 +0100

    rolling back file

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.8.150:23704/News-app-BE-api/api",
+  baseURL: "http://192.168.8.150:23704/News-app-BE-api/",
   // baseURL: "https://news-site-app.onrender.com/api",
 });
 
