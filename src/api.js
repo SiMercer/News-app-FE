3e57631b3bb53abe581268a72a84d654bf7f3f70
SiMercer <na>
2025-07-03 22:29:14 +0100

    updated baseURL path

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.8.150:23704/News-app-BE-api/",
+  baseURL: "https://simonmercer.co.uk:23707/News-app-BE-api/",
   // baseURL: "https://news-site-app.onrender.com/api",
 });
 
