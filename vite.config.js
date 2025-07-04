99462d113b14c4cd250d6b2e7699e655d3d401d6
SiMercer <simon@mercer.net>
2025-07-05 00:15:23 +0100

    chaged base to /

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/News-app-FE/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 });
